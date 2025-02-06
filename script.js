0a7b4e2ff274dc160151379efc82068327ce1ebb
Anil Kumar <114513976+ak8058@users.noreply.github.com>
2025-02-06 09:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let myBtn=document.getElementById("enterBtn");
+myBtn.addEventListener("click",()=>{
+	const statuscont=document.getElementById("status");
+	statuscont.innerHTML="<h1>Entered Metaverse</h1>"
+});
